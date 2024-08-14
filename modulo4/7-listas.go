e9d8efda2484c3da08acb1123588e46947585673
Montejohn <jonatasmonte23@gmail.com>
2024-08-14 08:01:34 -0300

    adcionando alguns comentarios explicativos

--- modulo4/7-listas.go
+++ modulo4/7-listas.go
@@ -5,43 +5,43 @@ import "fmt"
 func main() {
 
 	// Array - tamanho fixo
-	var array [2]string // var+array+tamanho_do_array+tipo
-	array[0] = "Hello"
-	array[1] = "World"
-	fmt.Println(array[0])
-	fmt.Println(array[1])
-	fmt.Println(array[0], array[1])
-	fmt.Println(array)
+	// var array [2]string // var+array+tamanho_do_array+tipo
+	// array[0] = "Hello"
+	// array[1] = "World"
+	// fmt.Println(array[0])
+	// fmt.Println(array[1])
+	// fmt.Println(array[0], array[1])
+	// fmt.Println(array)
 
-	numPrimos := [6]int{2, 3, 5, 7, 11, 13}
-	fmt.Println(numPrimos)
-	fmt.Println(numPrimos[0:4])
-	fmt.Println(numPrimos[0:6])
+	// numPrimos := [6]int{2, 3, 5, 7, 11, 13}
+	// fmt.Println(numPrimos)
+	// fmt.Println(numPrimos[0:4])
+	// fmt.Println(numPrimos[0:6])
 
 	//Slices
 	//var slice []string
-	//slice := make([]string, 5)
-	//slice[0] = "Hello"
-	//slice[1] = "World"
-	//fmt.Println(slice[0], slice[1])
-	//	fmt.Println(slice[0])
-	//fmt.Println(slice[1])
-	//fmt.Println(slice[2])
-	//slice[2] = "Steph"
-	//fmt.Println(slice[2])
-	//fmt.Println(slice)
+	// slice := make([]string, 5) // slice +:= função make ([]tipo, tamanho)
+	// slice[0] = "Hello"
+	// slice[1] = "World"
+	// fmt.Println(slice[0], slice[1])
+	// fmt.Println(slice[0])
+	// fmt.Println(slice[1])
+	// fmt.Println(slice[2])
+	// slice[2] = "Steph"
+	// fmt.Println(slice[2])
+	// fmt.Println(slice)
 
-	//fmt.Println(len(slice))
-	//fmt.Println(slice[2])
-	//	fmt.Println(slice[3])
-	//	fmt.Println(slice[4])
-	//	fmt.Println(slice[5])
+	// fmt.Println(len(slice))
+	// fmt.Println(slice[2])
+	// fmt.Println(slice[3])
+	// fmt.Println(slice[4])
+	// fmt.Println(slice[5])
 
-	//	numPares := []int{2, 4, 6, 8, 10, 12}
-	//	fmt.Println(numPares)
+	numPares := []int{2, 4, 6, 8, 10, 12} //criando um slice sem tamanho definido mas deve já ter valores no momento da criação
+	fmt.Println(numPares)
 
-	//	numPares = append(numPares, 14, 16, 18, 20)
-	//	fmt.Println(numPares)
+	numPares = append(numPares, 14, 16, 18, 20) //append serve para adicionar valores ao slice
+	fmt.Println(numPares)
 }
 
 // LISTAS
